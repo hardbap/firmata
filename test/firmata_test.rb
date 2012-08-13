72943106bba4ccaed033db735e8ec3d304d2602c
Mike Breen <hardbap@gmail.com>
2012-08-13 16:26:43 -0400

    Add default rake task and dummy test file.

---
+++ test/firmata_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative '../lib/firmata'
+
+class FirmataTest < MiniTest::Unit::TestCase
+
+  def test_something_dumbass
+    flunk
+  end
+end
