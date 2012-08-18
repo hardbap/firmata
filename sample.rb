9727eee336ad2fed9faf39c83de97a18a4d08ac3
Mike Breen <hardbap@gmail.com>
2012-08-17 22:39:19 -0400

    Fix up sample.rb.

--- sample.rb
+++ sample.rb
@@ -3,23 +3,14 @@ require 'firmata'
 
 board = Firmata::Board.new('/dev/tty.usbmodemfa131')
 
-board.on('ready', ->() do
-
-  10.times do
-    board.digital_write 13, Firmata::Board::HIGH
-    board.delay 1
-
-    board.digital_write 13, Firmata::Board::LOW
-    board.delay 1
-  end
-
-end)
-
 board.connect
 
- Thread.new do
-  loop do
-    board.read
-    sleep 1
-  end
+10.times do
+  board.digital_write 13, Firmata::Board::HIGH
+  puts '+'
+  board.delay 0.5
+
+  board.digital_write 13, Firmata::Board::LOW
+  puts '-'
+  board.delay 0.5
 end
