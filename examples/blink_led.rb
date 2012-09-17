118c32ed41a50fcb0a8b2dfa11b4a610e99fb2e6
Mike Breen <hardbap@gmail.com>
2012-09-17 10:08:31 -0400

    Move example to pin 3.

--- examples/blink_led.rb
+++ examples/blink_led.rb
@@ -1,16 +1,18 @@
 require 'bundler/setup'
 require 'firmata'
 
-board = Firmata::Board.new('/dev/tty.usbmodemfa131')
+board = Firmata::Board.new('/dev/tty.usbmodemfd13131')
 
 board.connect
 
+pin_number = 3
+
 10.times do
-  board.digital_write 13, Firmata::Board::HIGH
+  board.digital_write pin_number, Firmata::Board::HIGH
   puts '+'
   board.delay 0.5
 
-  board.digital_write 13, Firmata::Board::LOW
+  board.digital_write pin_number, Firmata::Board::LOW
   puts '-'
   board.delay 0.5
 end
