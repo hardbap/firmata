e902b67159d0dddc0eb2c1f061e6bde8bad3fdca
Mike Breen <hardbap@gmail.com>
2012-09-19 09:25:44 -0400

    Add rate var for blink example.

--- examples/blink_led.rb
+++ examples/blink_led.rb
@@ -6,13 +6,14 @@ board = Firmata::Board.new('/dev/tty.usbmodemfd13131')
 board.connect
 
 pin_number = 3
+rate = 0.5
 
 10.times do
   board.digital_write pin_number, Firmata::Board::HIGH
   puts '+'
-  board.delay 0.5
+  board.delay rate
 
   board.digital_write pin_number, Firmata::Board::LOW
   puts '-'
-  board.delay 0.5
+  board.delay rate
 end
