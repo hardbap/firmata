f7b636b21ba3f4639b89c319d6d3fca12221d0c6
deadprogrammer <ron.evans@gmail.com>
2013-02-17 09:27:46 -0800

    Adding digital read example

---
+++ examples/digital_read.rb
@@ -0,0 +1,26 @@
+require 'bundler/setup'
+require 'firmata'
+require 'socket'
+
+sp = TCPSocket.open 'localhost', 4567
+#sp = '/dev/tty.usbserial-A700636n'
+board = Firmata::Board.new(sp)
+
+board.connect
+
+puts "Firmware name #{board.firmware_name}"
+puts "Firmata version #{board.version}"
+
+pin_number = 2
+rate = 0.5
+
+listener = ->(pin, value) { puts("#{pin}:#{value}") }
+board.on('digital-read', listener)
+board.set_pin_mode(pin_number, Firmata::Board::INPUT)
+board.toggle_pin_reporting(pin_number)
+
+while true do
+	puts "waiting..."
+	board.read_and_process
+	sleep 0.5
+end
